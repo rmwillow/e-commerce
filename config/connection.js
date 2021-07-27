4dadc20cb79a7f00fecb016d2d866e59bb412d92
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-27 13:09:23 -0500

    setting up user routes and product tag columns

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
 
+
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,50 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+   // define an id column
+   id: {
+    // use the special Sequelize DataTypes object provide what type of data it is
+    type: DataTypes.INTEGER,
+    // this is the equivalent of SQL's `NOT NULL` option
+    allowNull: false,
+    // instruct that this is the Primary Key
+    primaryKey: true,
+    // turn on auto increment
+    autoIncrement: true
+  },
+  // define an product_name column
+  product_name: {
+    // use the special Sequelize DataTypes object provide what type of data it is
+    type: DataTypes.STRING,
+    // this is the equivalent of SQL's `NOT NULL` option
+    allowNull: false,
+  },
+
+  // define an price column
+  price: {
+    // use the special Sequelize DataTypes object provide what type of data it is
+    type: DataTypes.DECIMAL,
+    // this is the equivalent of SQL's `NOT NULL` option
+    allowNull: false,
+    // instruct that this is the Primary Key
+    isDecimal: true
+  },
+
+  // define an STOCK column
+  stock: {
+    // use the special Sequelize DataTypes object provide what type of data it is
+    type: DataTypes.INTEGER,
+    // this is the equivalent of SQL's `NOT NULL` option
+    allowNull: false,
+    // instruct that this is the Primary Key
+    defaultValue: "10",
+    // validates numeric value
+    isNumeric: true,
+    references: {
+      model: 'category',
+      key: 'id'
+    }
+  },
   },
   {
     sequelize,
