ba2035f1dd09d29f02a72f9048c9bcffb647a008
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-27 13:16:24 -0500

    successfully POST user login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const bcrypt = require('bcrypt');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -9,9 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
