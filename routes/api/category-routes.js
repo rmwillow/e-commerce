5ece7ffe1eb0c137c37b0fa6b70d73c87731bf4f
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-28 11:54:36 -0500

    category routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,53 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({})
+  .then(getResult => res.json(getResult))
+  .catch(err => res.json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(getResult => res.json(getResult))
+  .catch(err => res.json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+  .then(postResult => res.json(postResult))
+  .catch(err => res.json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(putResult => res.json(putResult))
+  .catch(err => res.json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(delResult => res.json(delResult))
+  .catch(err => res.json(err))
 });
 
 module.exports = router;
